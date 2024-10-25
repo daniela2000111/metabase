timuric/metabase-1

sankey prototype

frontend/src/metabase/static-viz/components/StaticVisualization/StaticVisualization.tsx
@@ -3,6 +3,7 @@ import type { StaticVisualizationProps } from "metabase/visualizations/types";
 
 import { ComboChart } from "../ComboChart";
 import { FunnelBarChart } from "../FunnelBarChart";
+import { SankeyChart } from "../SankeyChart";
 import { ScalarChart } from "../ScalarChart";
 import { ScatterPlot } from "../ScatterPlot/ScatterPlot";
 import { SmartScalar } from "../SmartScalar";
@@ -31,6 +32,8 @@ export const StaticVisualization = (props: StaticVisualizationProps) => {
       return <ScalarChart {...staticVisualizationProps} />;
     case "smartscalar":
       return <SmartScalar {...staticVisualizationProps} />;
+    case "sankey":
+      return <SankeyChart {...staticVisualizationProps} />;
   }
 
   throw new Error(`Unsupported display type: ${display}`);

daniela2000111/metabase

frontend/src/metabase/static-viz/components/StaticVisualization/StaticVisualization.tsx
@@ -6,6 +6,7 @@ import type { StaticVisualizationProps } from "metabase/visualizations/types";
 import { ComboChart } from "../ComboChart";
 import { FunnelBarChart } from "../FunnelBarChart";
 import { PieChart } from "../PieChart/PieChart";
+import { SankeyChart } from "../SankeyChart";
 import { ScalarChart } from "../ScalarChart";
 import { ScatterPlot } from "../ScatterPlot/ScatterPlot";
 import { SmartScalar } from "../SmartScalar";
@@ -46,6 +47,8 @@ export const StaticVisualization = ({
       return <SmartScalar {...props} />;
     case "pie":
       return <PieChart {...props} />;
+    case "sankey":
+      return <SankeyChart {...props} />;
   }
 
   throw new Error(`Unsupported display type: ${display}`);